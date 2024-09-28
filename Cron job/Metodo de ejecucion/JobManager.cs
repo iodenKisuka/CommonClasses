73ed720f0ababfba356952acf99d255ec641aaa0
Daiana Lorena Cabrera Saldivar <iodenteken@hotmail.com>
2024-09-28 09:23:16 -0300

    feat: add new cronjob for scheduled tasks

---
+++ Cron job/Metodo de ejecucion/JobManager.cs
@@ -0,0 +1,22 @@
+﻿using Quartz;
+using System.Threading.Tasks;
+
+namespace CommonClass.MetododeEjecucion
+{
+    public class JobManager : IJob
+    {
+        private static ProcessAutomatic _process;
+
+        public static void JobManagerVariable(ProcessAutoatic process)
+        {
+            _process = process;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _ = _process.Ejecution();
+            return Task.FromResult(true);
+        }
+
+    }
+}
---
+++ Cron job/Metodo de ejecucion/ServiceManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommonClass.MetododeEjecucion
+{
+    public class ServiceManager : BackgroundService
+    {
+        private readonly IConfiguration _config;
+        private readonly ProcessAutomatic _process;
+        private readonly ILogger<ServiceManager> _log;
+        public ServiceManager(IConfiguration config, ILogger<ServiceManager> logg, ProcessAutomatic process)
+        {
+            _config = config;
+            _process = process;
+            _log = logg;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                StdSchedulerFactory factory = new StdSchedulerFactory();
+                IScheduler scheduler = await factory.GetScheduler();
+                await scheduler.Start();
+                JobManager.JobManagerVariable(_caso);
+
+                var job = JobBuilder.Create<JobManager>()
+                 .Build();
+
+                var trigger = TriggerBuilder.Create()
+                .StartNow()
+                .WithCronSchedule(_config["horarioDeEjecucion"])
+                .Build();
+
+                await scheduler.ScheduleJob(job, trigger);
+
+                //linea 45 de la clase serviceManager... descomentala si es que necesitas probar poniendo play sin necesidad de esperar el job
+                await _process.Ejecution();
+
+            }
+            catch (Exception ex)
+            {
+                _log.LogError("ServiceManager - {ex.Message}", ex.Message);
+            }
+        }
+
+    }
+}
